23b7caffa525ab2efca69e684cac08a39f75c53d
omar <omarkader701@gmail.com>
2023-10-29 20:18:37 +0100

    commit all the code

---
+++ configuration-server_/src/test/java/com/omar/kader/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.kader.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
